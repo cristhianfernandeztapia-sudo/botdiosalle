5bd2c3b095d63903ff98f30c87944e4f2a6f4d91
cristhianfernandeztapia-sudo <cristhian.fernandez.tapia@gmail.com>
2025-08-23 18:22:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+import os
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
