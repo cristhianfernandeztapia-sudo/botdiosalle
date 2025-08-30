f7ac00ad0686033569e59aca2f8405b585f45c66
cristhianfernandeztapia-sudo <cristhian.fernandez.tapia@gmail.com>
2025-08-30 16:51:12 -0400

    Update cron_lia.py

--- cron_lia.py
+++ cron_lia.py
@@ -1,18 +1,22 @@
 import os
 import requests
-from estilos import frase_aleatoria
+import random
+from estilos import FRASES_PROACTIVAS
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 TELEGRAM_USER_ID = os.getenv("TELEGRAM_USER_ID")
 
-def enviar_mensaje(mensaje):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {
+URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+def frase_aleatoria():
+    return random.choice(FRASES_PROACTIVAS)
+
+def enviar_mensaje():
+    mensaje = frase_aleatoria()
+    requests.post(URL, json={
         "chat_id": TELEGRAM_USER_ID,
         "text": mensaje
-    }
-    requests.post(url, json=payload)
+    })
 
 if __name__ == "__main__":
-    mensaje = frase_aleatoria()
-    enviar_mensaje(mensaje)
+    enviar_mensaje()
