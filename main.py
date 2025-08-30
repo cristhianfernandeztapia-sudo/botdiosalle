429434d4406844dd33eae878bcfff5587c5bcafc
cristhianfernandeztapia-sudo <cristhian.fernandez.tapia@gmail.com>
2025-08-30 19:47:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 from utils import gpt  # ← corregido aquí
 from estilos import PERSONALIDAD_LIA
 from memoria import cargar_memoria, guardar_memoria
+from fastapi import FastAPI
 
 app = FastAPI()
 
